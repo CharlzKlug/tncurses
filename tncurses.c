3358f67d9048a6dc4f94174923150fe3cb949a3a
Ametov Imil Ilimdarovich <charlz_klug@mail.ru>
2024-03-14 01:23:48 +0300

    First commit

---
+++ tncurses.c
@@ -0,0 +1,37 @@
+/* How to build: gcc -shared -o libhello.so -DUSE_TCL_STUBS -I /usr/include/tcl hello.c -ltclstub8.6 -lncurses -fPIC */
+#include <tcl.h>
+#include <ncurses.h>
+
+static int 
+Hello_Cmd(ClientData cdata, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[])
+{
+  Tcl_SetObjResult(interp, Tcl_NewStringObj("Hello, World!", -1));
+  return TCL_OK;
+}
+
+static int Initscr_Cmd(ClientData cdata, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[]) {
+  initscr();
+  printw("hello!!!\n");
+  refresh();
+  getch();
+  endwin();
+  
+  Tcl_SetObjResult(interp, Tcl_NewStringObj("", -1));
+  return TCL_OK;
+}
+
+/*
+ * Hello_Init -- Called when Tcl loads your extension.
+ */
+int DLLEXPORT Hello_Init(Tcl_Interp *interp) {
+  if (Tcl_InitStubs(interp, TCL_VERSION, 0) == NULL) {
+    return TCL_ERROR;
+  }
+  /* changed this to check for an error - GPS */
+  if (Tcl_PkgProvide(interp, "Hello", "1.0") == TCL_ERROR) {
+    return TCL_ERROR;
+  }
+  Tcl_CreateObjCommand(interp, "hello", Hello_Cmd, NULL, NULL);
+  Tcl_CreateObjCommand(interp, "initscr", Initscr_Cmd, NULL, NULL);
+  return TCL_OK;
+}
