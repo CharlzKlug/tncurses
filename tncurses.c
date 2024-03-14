619fec21c718a9492526740af87cec5cb1d589cb
Imil Ametov <charlz_klug@mail.ru>
2024-03-14 14:08:36 +0300

    Added newwin, addstr, mvwaddstr, noecho, wrefresh, touchwin

--- tncurses.c
+++ tncurses.c
@@ -38,6 +38,94 @@ static int EndWin_Cmd(ClientData cdata, Tcl_Interp *interp, int objc, Tcl_Obj *c
   return TCL_OK;
 }
 
+static int NewWin_Cmd(ClientData cdata, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[]) {
+  if (objc != 5) {
+    return TCL_ERROR;
+  }
+  int rows;
+  Tcl_GetIntFromObj(interp, objv[1], &rows);
+  int cols;
+  Tcl_GetIntFromObj(interp, objv[2], &cols);
+  int y_org;
+  Tcl_GetIntFromObj(interp, objv[3], &y_org);
+  int x_org;
+  Tcl_GetIntFromObj(interp, objv[4], &x_org);
+  WINDOW *window= newwin(rows, cols, y_org, x_org);
+
+  char hexstr[24];
+  sprintf(hexstr, "%p", (void*)&window);
+  Tcl_SetObjResult(interp, Tcl_NewStringObj(hexstr, -1));
+  return TCL_OK;
+}
+
+static int AddStr_Cmd(ClientData cdata, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[]) {
+  if (objc != 2) {
+    return TCL_ERROR;
+  }
+  addstr(Tcl_GetString(objv[1]));
+  Tcl_SetObjResult(interp, Tcl_NewStringObj("", -1));
+  return TCL_OK;
+}
+
+static int MvWAddStr_Cmd(ClientData cdata, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[]) {
+  if (objc != 5) {
+    return TCL_ERROR;
+  }
+  char* buffer= Tcl_GetString(objv[1]);
+  void* pointer= NULL;
+  if (buffer == NULL || sscanf(buffer, "%p", &pointer) != 1) {
+    Tcl_AppendResult(interp, "Bad scan", NULL);
+    return TCL_ERROR;
+  }
+  WINDOW* win= (WINDOW*)pointer;
+  int y_org;
+  Tcl_GetIntFromObj(interp, objv[2], &y_org);
+  int x_org;
+  Tcl_GetIntFromObj(interp, objv[3], &x_org);
+  char* strbuf= Tcl_GetString(objv[4]);
+  mvwaddstr(win, y_org, x_org, strbuf);
+  Tcl_SetObjResult(interp, Tcl_NewStringObj("", -1));
+  return TCL_OK;
+}
+
+static int NoEcho_Cmd(ClientData cdata, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[]) {
+  noecho();
+  Tcl_SetObjResult(interp, Tcl_NewStringObj("", -1));
+  return TCL_OK;
+}
+
+static int WRefresh_Cmd(ClientData cdata, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[]) {
+  if (objc != 2) {
+    return TCL_ERROR;
+  }
+  char* buffer= Tcl_GetString(objv[1]);
+  void* pointer= NULL;
+  if (buffer == NULL || sscanf(buffer, "%p", &pointer) != 1) {
+    Tcl_AppendResult(interp, "Bad scan", NULL);
+    return TCL_ERROR;
+  }
+  WINDOW* win= (WINDOW*)pointer;
+  wrefresh(win);
+  Tcl_SetObjResult(interp, Tcl_NewStringObj("", -1));
+  return TCL_OK;
+}
+
+static int TouchWin_Cmd(ClientData cdata, Tcl_Interp *interp, int objc, Tcl_Obj *const objv[]) {
+  if (objc != 2) {
+    return TCL_ERROR;
+  }
+  char* buffer= Tcl_GetString(objv[1]);
+  void* pointer= NULL;
+  if (buffer == NULL || sscanf(buffer, "%p", &pointer) != 1) {
+    Tcl_AppendResult(interp, "Bad scan", NULL);
+    return TCL_ERROR;
+  }
+  WINDOW* win= (WINDOW*)pointer;
+  touchwin(win);
+  Tcl_SetObjResult(interp, Tcl_NewStringObj("", -1));
+  return TCL_OK;
+}
+
 int DLLEXPORT Tncurses_Init(Tcl_Interp *interp) {
   if (Tcl_InitStubs(interp, TCL_VERSION, 0) == NULL) {
     return TCL_ERROR;
@@ -51,5 +139,11 @@ int DLLEXPORT Tncurses_Init(Tcl_Interp *interp) {
   Tcl_CreateObjCommand(interp, "refresh", Refresh_Cmd, NULL, NULL);
   Tcl_CreateObjCommand(interp, "getch", GetCh_Cmd, NULL, NULL);
   Tcl_CreateObjCommand(interp, "endwin", EndWin_Cmd, NULL, NULL);
+  Tcl_CreateObjCommand(interp, "newwin", NewWin_Cmd, NULL, NULL);
+  Tcl_CreateObjCommand(interp, "addstr", AddStr_Cmd, NULL, NULL);
+  Tcl_CreateObjCommand(interp, "mvwaddstr", MvWAddStr_Cmd, NULL, NULL);
+  Tcl_CreateObjCommand(interp, "noecho", NoEcho_Cmd, NULL, NULL);
+  Tcl_CreateObjCommand(interp, "wrefresh", WRefresh_Cmd, NULL, NULL);
+  Tcl_CreateObjCommand(interp, "touchwin", WRefresh_Cmd, NULL, NULL);
   return TCL_OK;
 }
