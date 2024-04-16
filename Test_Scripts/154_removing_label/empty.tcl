49377df48a19948326243ec32d0208a8984b49a2
Imil Ametov <charlz_klug@mail.ru>
2024-04-16 09:41:16 +0300

    Added removing softlabel script example

---
+++ Test_Scripts/154_removing_label/empty.tcl
@@ -0,0 +1,26 @@
+#!/usr/bin/env tclsh
+
+load ./libtncurses.so
+
+set LMAX 8
+set CENTER 1
+
+set label_text {
+    "Duck" "Duck" "Duck" "Duck"
+    "Duck" "Duck" "Duck" "Duck"
+}
+
+slk_init 1
+initscr
+
+for {set label 0} {$label < $LMAX} {incr label} {
+    slk_set [expr {$label + 1}] [lindex $label_text $label] $CENTER
+}
+slk_refresh
+getch
+
+slk_set 7 "" $CENTER
+slk_refresh
+getch
+
+endwin
