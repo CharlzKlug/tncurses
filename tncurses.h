0f7d1f67ac09c2834f9f01e77f303f820843634a
Imil Ametov <charlz_klug@mail.ru>
2024-05-16 10:23:32 +0300

    Auxiliary macroses and functions removed to the tncurses.h

--- tncurses.h
+++ tncurses.h
@@ -26,4 +26,64 @@ NCURSES_COLOR_T ncurses_colors[(COLORS_COUNT)] = {
   COLOR_WHITE
 };
 
+#define CHECK_ARGUMENTS(arg_num, err_msg) do {		\
+    if (objc != (arg_num)) {				\
+      Tcl_AppendResult(interp, (err_msg), NULL);	\
+      return TCL_ERROR;					\
+    }							\
+  } while (0)
+
+#define STRING_TO_WINDOW(string_buffer, window) do {			\
+    if (strcmp(string_buffer, "stdscr") == 0) {				\
+      window= stdscr;							\
+    } else {								\
+      void* pointer= NULL;						\
+      if (string_buffer == NULL ||					\
+	  sscanf(string_buffer, "%p", &pointer) != 1) {			\
+	Tcl_AppendResult(interp, "Bad scan", NULL);			\
+	return TCL_ERROR;						\
+      }									\
+      window= (WINDOW*)pointer;						\
+    }									\
+  } while (0)
+
+#define STRING_TO_BOOL(string_buffer, bool_value) do {		\
+    if (strcmp(string_buffer, "true") == 0 ||			\
+	strcmp(string_buffer, "TRUE") == 0) {			\
+      bool_value= TRUE;						\
+    } else if (strcmp(string_buffer, "false") == 0 ||		\
+	       strcmp(string_buffer, "FALSE") == 0) {		\
+      bool_value= FALSE;					\
+    } else {							\
+      Tcl_AppendResult(interp, "Bad scan bool value", NULL);	\
+      return TCL_ERROR;						\
+    }								\
+  } while (0)
+
+#define STRING_TO_MMASK_T(string_buffer, somemask) do {			\
+    if (strcmp(string_buffer, "NULL") == 0) {				\
+      somemask= NULL;							\
+    } else {								\
+      void* pointer= NULL;						\
+      if (string_buffer == NULL ||					\
+	  sscanf(string_buffer, "%p", &pointer) != 1) {			\
+	Tcl_AppendResult(interp, "Bad scan", NULL);			\
+	return TCL_ERROR;						\
+      }									\
+      somemask= (mmask_t*)pointer;					\
+    }									\
+  } while (0)
+
+/* Auxiliary function. Maps string to color */
+int string_to_color(char *input_string,
+		    NCURSES_COLOR_T *output_color) {
+  for (int i = 0; i < (COLORS_COUNT); i++) {
+    if (strcmp(input_string, colors_strings[i]) == 0) {
+      *output_color= ncurses_colors[i];
+      return i;
+    }
+  }
+  return (-1);
+}
+
 #endif /* TNCURSES_H */
