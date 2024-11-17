a368d0bbcb49da745e8ff7cab49098e9704db6df
Imil Ametov <charlz_klug@mail.ru>
2024-11-17 11:07:00 +0300

    Rewrited 'kid2.tcl' script

--- Test_Scripts/109_second_subwindow/kid2.tcl
+++ Test_Scripts/109_second_subwindow/kid2.tcl
@@ -5,9 +5,9 @@ package require Tncurses
 
 tncurses::initscr
 tncurses::start_color
-tncurses::init_pair 1 COLOR_WHITE COLOR_BLUE
-tncurses::init_pair 2 COLOR_RED COLOR_YELLOW
-tncurses::init_pair 3 COLOR_CYAN COLOR_WHITE
+tncurses::init_pair 1 [tncurses::COLOR_WHITE] [tncurses::COLOR_BLUE]
+tncurses::init_pair 2 [tncurses::COLOR_RED] [tncurses::COLOR_YELLOW]
+tncurses::init_pair 3 [tncurses::COLOR_CYAN] [tncurses::COLOR_WHITE]
 
 set sonny [tncurses::subwin stdscr 5 20 10 30]
 set babygirl [tncurses::derwin stdscr 5 20 1 50]
@@ -20,6 +20,8 @@ tncurses::waddstr $sonny "Hello, Dad."
 tncurses::wbkgd $babygirl [tncurses::COLOR_PAIR 3]
 tncurses::waddstr $babygirl "Hello, Papa."
 tncurses::refresh
+tncurses::wrefresh $sonny
+tncurses::wrefresh $babygirl
 tncurses::getch
 
 tncurses::endwin
